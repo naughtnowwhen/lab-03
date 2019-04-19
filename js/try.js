0e8669383e36fb0ba26b5333b8d8b28bbf15c2ef
Peter Murphy <naughtnowwhen@gmail.com>
2019-04-18 17:49:09 -0700

    erased js

--- js/try.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let nums = [12, 123, 542, 8, 9, 123, 45, 1112, 12345, 1453124];
-const sortNumbersByLength = (arr) => {
-    arr.sort((a, b) => (a.toString().length > b.toString().length ? 1 : 0));
-    return arr;
-};
-sortNumbersByLength(nums); //?
-//# sourceMappingURL=try.js.map
